83e861e22d6c3e682e93f231d9d1799afe0e9db4
Romain Lenglet <romain@sgnl.ai>
2023-06-17 07:09:45 +0000

    Implement JSON attribute value parsing

---
+++ web/json_object.go
@@ -0,0 +1,116 @@
+// Copyright 2023 SGNL.ai, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package web
+
+import (
+	"fmt"
+
+	framework "github.com/sgnl-ai/adapter-framework"
+)
+
+// ConvertJSONObjectList parses and converts a list of JSON objects received
+// from the given requested entity.
+func ConvertJSONObjectList(entity *framework.EntityConfig, objects []map[string]any) ([]framework.Object, error) {
+	if len(objects) == 0 {
+		return nil, nil
+	}
+
+	parsedObjects := make([]framework.Object, 0, len(objects))
+
+	for _, object := range objects {
+		parsedObject, err := ConvertJSONObject(entity, object)
+
+		if err != nil {
+			return nil, err
+		}
+
+		parsedObjects = append(parsedObjects, parsedObject)
+	}
+
+	return parsedObjects, nil
+}
+
+// ConvertJSONObject parses and converts a JSON object received from the given
+//requested entity.
+func ConvertJSONObject(entity *framework.EntityConfig, object map[string]any) (framework.Object, error) {
+	parsedObject := make(framework.Object)
+
+	// Parse attributes.
+	for _, attribute := range entity.Attributes {
+		externalId := attribute.ExternalId
+
+		// TODO: Support flattening single-valued complex attributes,
+		// e.g. flatten this JSON object:
+		//
+		// {
+		//   "manager": {
+		//     "id": "1234",
+		//     "email": "john@example.com"
+		//   }
+		// }
+		//
+		// into:
+		//
+		// {
+		//   "manager__id": "1234",
+		//   "manager__email": "john@example.com"
+		// }
+
+		value, found := object[externalId]
+		if !found {
+			continue
+		}
+
+		parsedValue, err := ConvertJSONAttributeValue(attribute, value)
+		if err != nil {
+			return nil, err
+		}
+
+		// Do not return null attribute values.
+		if parsedValue == nil {
+			continue
+		}
+
+		parsedObject[externalId] = parsedValue
+	}
+
+	// Parse child entities.
+	for _, childEntity := range entity.ChildEntities {
+		externalId := childEntity.ExternalId
+
+		childObjectsRaw, found := object[externalId]
+		if !found {
+			continue
+		}
+
+		childObjects, ok := childObjectsRaw.([]map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("child entity %s is not associated with a list of JSON objects", externalId)
+		}
+
+		if len(childObjects) == 0 {
+			continue
+		}
+
+		parsedChildObjects, err := ConvertJSONObjectList(childEntity, childObjects)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse objects for child entity %s: %w", externalId, err)
+		}
+
+		parsedObject[externalId] = parsedChildObjects
+	}
+
+	return parsedObject, nil
+}
---
+++ web/json_value.go
@@ -0,0 +1,171 @@
+// Copyright 2023 SGNL.ai, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package web
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	framework "github.com/sgnl-ai/adapter-framework"
+)
+
+var (
+	// SupportedDateTimeFormats is the list of date-time formats that can be
+	// parsed by ParseDateTime.
+	// The formats are ordered by assumed decreasing order of likelihood.
+	SupportedDateTimeFormats = []string{
+		time.RFC3339, time.RFC3339Nano,
+		time.RFC1123, time.RFC1123Z,
+		time.RFC822, time.RFC822Z, time.RFC850,
+		time.UnixDate, time.RubyDate, time.ANSIC,
+		"2006-01-02T15:04:05.000Z0700",
+		"2006-01-02 15:04:05",
+	}
+)
+
+// ConvertJSONAttributeValue parses and converts the value of a JSON object
+// field.
+func ConvertJSONAttributeValue(attribute *framework.AttributeConfig, value any) (any, error) {
+	if value == nil {
+		return nil, nil
+	}
+
+	if attribute.List {
+		list, ok := value.([]any)
+		if !ok {
+			return nil, fmt.Errorf("list attribute %s cannot be parsed into a list value", attribute.ExternalId)
+		}
+
+		if len(list) == 0 {
+			return list, nil
+		}
+
+		parsedList := make([]any, 0, len(list))
+
+		elementAttribute := *attribute
+		elementAttribute.List = false
+
+		for _, element := range list {
+			parsedElement, err := ConvertJSONAttributeValue(&elementAttribute, element)
+
+			if err != nil {
+				return nil, err
+			}
+
+			// Do not return null attribute values.
+			if parsedElement == nil {
+				continue
+			}
+
+			parsedList = append(parsedList, parsedElement)
+		}
+
+		return parsedList, nil
+	}
+
+	switch attribute.Type {
+
+	case framework.AttributeTypeBool:
+		var boolValue bool
+		switch v := value.(type) {
+		case bool:
+			boolValue = v
+		case string:
+			lowerCaseValue := strings.ToLower(v)
+			switch lowerCaseValue {
+			case "false", "0":
+				boolValue = false
+			case "true", "1":
+				boolValue = true
+			default:
+				return nil, fmt.Errorf("attribute %s cannot be parsed into a bool value", attribute.ExternalId)
+			}
+		case float64:
+			intValue := int(v)
+			switch intValue {
+			case 0:
+				boolValue = false
+			case 1:
+				boolValue = true
+			default:
+				return nil, fmt.Errorf("attribute %s cannot be parsed into a bool value", attribute.ExternalId)
+			}
+		}
+		return boolValue, nil
+
+	case framework.AttributeTypeDateTime:
+		v, ok := value.(string)
+		if !ok {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into a string date-time value", attribute.ExternalId)
+		}
+		if v == "" {
+			return nil, nil
+		}
+		t, err := ParseDateTime(v)
+		if err != nil {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into a date-time value: %w", attribute.ExternalId, err)
+		}
+		return t, nil
+
+	case framework.AttributeTypeDouble:
+		v, ok := value.(float64)
+		if !ok {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into a float64 value", attribute.ExternalId)
+		}
+		return v, nil
+
+	case framework.AttributeTypeDuration:
+		// Duration is assumed to be a number of seconds, as a float64,
+		// possibly with a fractional part, e.g.: 3.5.
+		v, ok := value.(float64)
+		if !ok {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into a float64 duration value", attribute.ExternalId)
+		}
+		return time.Duration(v * float64(time.Second)), nil
+
+	case framework.AttributeTypeInt64:
+		// All numbers are unmarshalled into float64. Convert into int64.
+		v, ok := value.(float64)
+		if !ok {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into an int64 value", attribute.ExternalId)
+		}
+		return v, nil
+
+	case framework.AttributeTypeString:
+		v, ok := value.(string)
+		if !ok {
+			return nil, fmt.Errorf("attribute %s cannot be parsed into a string value", attribute.ExternalId)
+		}
+		return v, nil
+
+	default:
+		panic("invalid attribute type")
+	}
+}
+
+// ParseDateTime parses a timestamp against a set of predefined formats.
+func ParseDateTime(dateTimeStr string) (dateTime time.Time, err error) {
+	for _, format := range SupportedDateTimeFormats {
+		dateTime, err = time.Parse(format, dateTimeStr)
+		if err == nil {
+			return
+		}
+	}
+
+	err = fmt.Errorf("failed to parse date-time value: %s", dateTimeStr)
+
+	return
+}
